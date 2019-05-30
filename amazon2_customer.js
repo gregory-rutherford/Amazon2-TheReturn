9b0dca3b43a9e1f9c6118987c22418e8e035ee35
Gregory Rutherford <gregorford@gmail.com>
2019-05-30 14:53:16 -0500

    added sql file for initializing and finalized manager view

--- amazon2_customer.js
+++ amazon2_customer.js
@@ -1,5 +1,4 @@
-var colors = require('colors');
-
+var colors = require("colors");
 
 console.log("Welcome to Amazon part 2, The Return!".black.bgCyan.underline);
 
@@ -25,7 +24,7 @@ function displayDB() {
         if (err) throw err;
         console.table(res);
         afterConnection();
-    })
+    });
 }
 
 connection.connect(function (err) {
@@ -33,75 +32,61 @@ connection.connect(function (err) {
         throw err;
     }
     displayDB();
-
-})
-
+});
 
 function afterConnection() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["Shop", "Exit"],
-            name: "input"
-        }
-    ])
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Shop", "Exit"],
+                name: "input"
+            }
+        ])
         .then(function (user) {
             if (user.input === "Exit") {
                 connection.end();
             } else {
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "choice",
-                        message: "Enter the item ID of the prodcut you would like to purchase",
-                    },
-                    {
-                        type: "input",
-                        name: "choice2",
-                        message: "How many would you like to buy?"
-                    }
-                ])
-                    .then(function (user) {
-                        connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [user.choice], function (err, res) {
-                            if (err) {
-                                throw err
-                            }
-                            else if (user.choice2 > res[0].stock_quantity) {
-                                console.log("Insufficient quantity!");
-                                afterConnection();
-                            }
-                            else {
-                                var storage = (res[0].stock_quantity - user.choice2);
-                                connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [storage, user.choice], function (err) {
-                                    if (err) throw err;
-                                });
-                                console.log("Wow very cool, your order is on the way!".cyan);
-                                displayDB();
-                            }
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "choice",
+                            message:
+                                "Enter the item ID of the prodcut you would like to purchase"
+                        },
+                        {
+                            type: "input",
+                            name: "choice2",
+                            message: "How many would you like to buy?"
                         }
-                        )
-                    })
+                    ])
+                    .then(function (user) {
+                        connection.query(
+                            "SELECT stock_quantity FROM products WHERE item_id = ?",
+                            [user.choice],
+                            function (err, res) {
+                                if (err) {
+                                    throw err;
+                                } else if (user.choice2 > res[0].stock_quantity) {
+                                    console.log("Insufficient quantity!");
+                                    afterConnection();
+                                } else {
+                                    var storage = res[0].stock_quantity - user.choice2;
+                                    connection.query(
+                                        "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+                                        [storage, user.choice],
+                                        function (err) {
+                                            if (err) throw err;
+                                        }
+                                    );
+                                    console.log("Wow very cool, your order is on the way!".cyan);
+                                    displayDB();
+                                }
+                            }
+                        );
+                    });
             }
-        })
-
+        });
 }
-
-
-
-
-//how to use tables from Brock.
-// var query = "select department_id, d.department_name, over_head_costs, ifnull(sum(product_sales), 0) as product_sales, ifnull((sum(product_sales) - over_head_costs),0) as total_profit from departments d left join products p on p.department_name = d.department_name group by department_id";
-//    connection.query(query, function (err, res) {
-//        if (err) throw err;
-//        res.map(i => itemCount.push(i.id));
-//        var showTable = new Table({
-//            head: ["department_id", "department_name", "over_head_costs", "product_sales", "total_profit"],
-//            colWidths: [20, 25, 20, 20, 20]
-//        });
-//        for (var i = 0; i < res.length; i++) {
-//            showTable.push(
-//                [res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit]
-//            );
-//        }
-//        console.log(showTable.toString());
--- managerview.js
+++ managerview.js
@@ -1,11 +1,19 @@
-var colors = require('colors');
-
-
+//colors for terminal
+var colors = require("colors");
+//setting up cli tables
+var Table = require("cli-table");
+var table = new Table({
+    head: ["item_ID", "Name", "Department", "Cost", "Quantity"],
+    colWidths: [10, 35, 12, 14, 14]
+});
+//welcome message
 console.log("Welcome to Amazon part 2, The Return!".black.bgCyan.underline);
-
+//setting up dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+//empty array for cli tables to push into
+var displayArr = [];
+//setting up connection
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -19,90 +27,172 @@ var connection = mysql.createConnection({
     password: "yourRootPassword",
     database: "amazon2"
 });
-
+//connection to DB and displaying table of product info
 function displayDB() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.table(res);
-        afterConnection();
-    })
-}
-
-function viewLow() {
-    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, res) {
-        if (err) throw err;
-        console.table(res);
-    })
-}
-
-
-function addToInv() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Please enter the item id number of the product you would like to add",
-            name: "item",
-        },
-        {
-            type: "input",
-            message: "Please enter the quantity that you would like to add",
-            name: "quantity",
-        }
-    ]).then(function (user2) {
-        var item = user2.item;
-        var quant = user2.quantity;
-        connection.query("SELECT * FROM products WHERE item_id = ?", [item], function (err, res) {
-            if (err)
-                throw err;
-            else {
-                var number = parseInt(quant);
-                var storage = (res[0].stock_quantity + number);
-                connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [storage, item], function (err, res) {
-                    if (err)
-                        throw err;
-                    displayDB();
-                    connection.end();
-                });
-            }
+        var table = new Table({
+            head: ["item_ID", "Name", "Department", "Cost", "Quantity"],
+            colWidths: [10, 35, 12, 14, 14]
         });
+        for (i = 0; i < res.length; i++) {
+            table.push([
+                res[i].item_id,
+                res[i].product_name,
+                res[i].department_name,
+                res[i].price,
+                res[i].stock_quantity
+            ]);
+        }
+        console.log(table.toString());
+        mainMenu();
     });
 }
 
 connection.connect(function (err) {
-    if (err) throw err;
-    displayDB();
-})
-
-
-function afterConnection() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
-            name: "input"
-        }
-    ])
+    if (err) {
+        throw err;
+    }
+    mainMenu();
+});
+//main screen in which user can access commands
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: [
+                    "View Products for Sale",
+                    "View Low Inventory",
+                    "Add to Inventory",
+                    "Add New Product",
+                    "Exit"
+                ],
+                name: "input"
+            }
+        ])
         .then(function (user) {
-
             switch (user.input) {
-                case ("Exit"):
+                case "Exit":
                     connection.end();
-                    break
-                case ("View Products for Sale"):
+                    break;
+                case "View Products for Sale":
                     displayDB();
-                    afterConnection();
-                    break
-                case ("View Low Inventory"):
+                    break;
+                case "View Low Inventory":
                     viewLow();
-                    afterConnection();
-                    break
-                case ("Add to Inventory"): 
-                    
+                    break;
+                case "Add to Inventory":
                     addToInv();
-                    break
+                    break;
+                case "Add New Product":
+                    addNew();
+                    break;
+                default:
+                    mainMenu();
+            }
+        });
+}
+//view items with quantity lower than 5
+function viewLow() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (
+        err,
+        res
+    ) {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            table.push([
+                res[i].item_id,
+                res[i].product_name,
+                res[i].department_name,
+                res[i].price,
+                res[i].stock_quantity
+            ]);
+        }
+        console.log(table.toString());
+        mainMenu();
+    });
+}
+//add to the inventory of the database for specific products
+function addToInv() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message:
+                    "Please enter the item id number of the product you would like to add",
+                name: "item"
+            },
+            {
+                type: "input",
+                message: "Please enter the quantity that you would like to add",
+                name: "quantity"
+            }
+        ])
+        .then(function (user2) {
+            var item = user2.item;
+            var quant = user2.quantity;
+            connection.query(
+                "SELECT * FROM products WHERE item_id = ?",
+                [item],
+                function (err, res) {
+                    if (err) throw err;
+                    else {
+                        var number = parseInt(quant);
+                        var storage = res[0].stock_quantity + number;
+                        connection.query(
+                            "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+                            [storage, item],
+                            function (err, res) {
+                                if (err) throw err;
+                                mainMenu();
+                            }
+                        );
+                    }
                 }
-        })
-};
-
-                
+            );
+        });
+}
+// add a new product into the db
+function addNew() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter the name of the product you would like to add",
+                name: "item"
+            },
+            {
+                type: "input",
+                message: "Please enter the items category",
+                name: "category"
+            },
+            {
+                type: "input",
+                message: "Please enter the items cost",
+                name: "cost"
+            },
+            {
+                type: "input",
+                message: "Please enter the amount of product",
+                name: "amount"
+            }
+        ])
+        .then(function (user) {
+            var newItem = user.item;
+            var newCat = user.category;
+            var newCost = user.cost;
+            var newAmount = user.amount;
+            var query =
+                "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?,?,?,?)";
+            connection.query(query, [newItem, newCat, newCost, newAmount], function (
+                err,
+                res
+            ) {
+                if (err) throw err;
+                console.log("Your item has been added.");
+                mainMenu();
+            });
+        });
+}
